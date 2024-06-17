7701ea7de9216fe15a51f72afe7d890cdb973ec8
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-06-17 14:34:39 +0000

    Install taplo in the devcontainer _image_
    Signed-off-by: Jonathan Giannuzzi <jonathan@giannuzzi.me>

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -3,6 +3,3 @@
 # Install additional go tools
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.33.0
 go install github.com/vektra/mockery/v2@v2.43.2
-
-# Install taplo
-npm install -g @taplo/cli
